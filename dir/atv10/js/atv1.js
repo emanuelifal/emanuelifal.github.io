4d073e10fb37f4454a34599ae7352f95567bffd1
emanuelifal <139375235+emanuelifal@users.noreply.github.com>
2023-10-16 15:00:05 -0300

    Create atv1.js

---
+++ dir/atv10/js/atv1.js
@@ -0,0 +1,5 @@
+function js_style() {
+    document.querySelector('#text').style.color = 'blue';
+    document.querySelector('#text').style.fontSize = '30px';
+    document.querySelector('#text').style.fontFamily = 'arial'
+}
