5e7121875b085d97a464d12942f11949176a6041
emanuelifal <139375235+emanuelifal@users.noreply.github.com>
2023-10-17 15:41:06 -0300

    Update atv2.js

--- dir/atv10/js/atv2.js
+++ dir/atv10/js/atv2.js
@@ -1,19 +1,23 @@
 let contador = 3
+
 function js_add_rows() {
-    let input = document.querySelector('#txtNumRows').value;
+    let input = parseInt(document.querySelector('#txtNumRows').value);
     const template = document.querySelector("#template");
-    if (filterInt(input) == 's'){
-        document.querySelector('#error').innerHTML = 'Só aceitamos valores numéricos.'
+
+    if (isNaN(input) || input < 1 || input > 10) {
+        document.querySelector('#error').innerHTML = 'Por favor, insira um valor entre 1 e 10.';
+        return;
     }
+
     for (let i = 0; i < input; i++) {
-        document.querySelector('#sampleTable').innerHTML += template.innerHTML = `<td>Linha${contador} celula1</td>` + `<td>Linha${contador} celula2</td>`;
+        document.querySelector('#sampleTable').innerHTML += `<tr><td>Linha${contador} celula1</td><td>Linha${contador} celula2</td></tr>`;
         contador++
     }
-}
 
+    document.querySelector('#error').innerHTML = '';
+}
 
 filterInt = function (value) {
     if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value)) return Number(value);
     return 's';
-  };
-    
+};
