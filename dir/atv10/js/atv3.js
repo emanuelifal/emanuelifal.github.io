74ab76c72dbc8d29182954fd6d75776ab7076934
emanuelifal <139375235+emanuelifal@users.noreply.github.com>
2023-10-17 15:42:39 -0300

    Update atv3.js

--- dir/atv10/js/atv3.js
+++ dir/atv10/js/atv3.js
@@ -1,6 +1,6 @@
 function contadorVogais(string) {
     let stringsplitada = string.split('')
-    let vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    let vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'á', 'é', 'í', 'ó', 'ú', 'à', 'è', 'ì', 'ò', 'ù', 'ã', 'ẽ', 'ĩ', 'õ', 'ũ', 'â', 'ê', 'î', 'ô', 'û', 'Á', 'É', 'Í', 'Ó', 'Ú', 'À', 'È', 'Ì', 'Ò', 'Ù', 'Ã', 'Ẽ', 'Ĩ', 'Õ', 'Ũ', 'Â', 'Ê', 'Î', 'Ô', 'Û']
     let contador = 0
     stringsplitada.forEach(letra => {
         vogais.forEach(vogal => {
@@ -11,7 +11,6 @@ function contadorVogais(string) {
     });
     return contador + ' vogais'
 }
-
 
 function main(){
     let conteudo = document.querySelector('input').value
